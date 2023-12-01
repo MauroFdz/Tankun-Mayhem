c20520ea602e9a7854863b40ced98d46c4119930
DanielBermejoValverde <43214261+DanielBermejoValverde@users.noreply.github.com>
2023-12-01 12:53:13 +0100

    ne color

--- Tankun-Mayhem/Scenes/GameScene.js
+++ Tankun-Mayhem/Scenes/GameScene.js
@@ -242,10 +242,10 @@ class GameScene extends Phaser.Scene
 		avion.setVelocity(0,-250);
 		
 		
-		this.score1Text = this.add.text(25, 635, 'Puntuación:0', { fontSize: '32px', fill: '#03d92d' });
-		this.score2Text = this.add.text(1300,635, 'Puntuación:0', { fontSize: '32px', fill: '#03d92d' });
+		this.score1Text = this.add.text(25, 635, 'Puntuación:0', { fontSize: '32px', fill: '#ff8c00' });
+		this.score2Text = this.add.text(1300,635, 'Puntuación:0', { fontSize: '32px', fill: '#ff8c00' });
 		this.startTime = new Date().getSeconds() + new Date().getMinutes()*60 + new Date().getHours()*3600;
-		this.timerText = this.add.text(780, 635,'', { fontSize: '32px', fill: '#03d92d' });
+		this.timerText = this.add.text(780, 635,'', { fontSize: '32px', fill: '#ff8c00' });
 			
 		
 	}
